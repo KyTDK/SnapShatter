60d5cb40a8bfba5320b265153d992076678fd96c
KyTDK <71435411+KyTDK@users.noreply.github.com>
2023-10-29 17:56:35 +1100

    first commit

---
+++ main.py
@@ -0,0 +1,243 @@
+import os
+import time
+import cv2
+import traceback
+from src.utils import find_image, get_device, get_device_dimensions
+
+device = get_device()
+
+import cv2
+import json
+
+
+def calibrate_scale(image_name):
+    input(f"Open snapchat page that contains {image_name}, then press enter")
+
+    # Capture the screenshot
+    image = device.screencap()
+
+    # Save the screenshot as 'screen.png'
+    with open("screen.png", "wb") as file:
+        file.write(image)
+
+    # Load the screenshot as an image in grayscale
+    screenshot = cv2.imread("screen.png", cv2.IMREAD_GRAYSCALE)
+
+    # Load the template image to be recognized in grayscale
+    template_image = cv2.imread(f"./images/{image_name}", cv2.IMREAD_GRAYSCALE)
+
+    # Initial scale and peak accuracy
+    scale = 0.1
+    peak_accuracy = 0
+
+    while True:
+        # Resize the template image
+        template_resized = cv2.resize(template_image, None, fx=scale, fy=scale)
+
+        # Check if the template image exceeds the size of the screenshot
+        if (
+            template_resized.shape[0] > screenshot.shape[0]
+            or template_resized.shape[1] > screenshot.shape[1]
+        ):
+            break  # Stop if the template image is too large
+
+        # Perform template matching at the current scale
+        result = cv2.matchTemplate(screenshot, template_resized, cv2.TM_CCOEFF_NORMED)
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+
+        # Check if the current accuracy is greater than the peak accuracy
+        if max_val > peak_accuracy:
+            print(f"New best accuracy: {max_val} at scale {scale}")
+            peak_accuracy = max_val
+            best_scale = scale
+
+        # Increase the scale for the next iteration
+        scale += 0.001
+
+    return best_scale, peak_accuracy
+
+
+def send_snaps():
+    try:
+        while True:
+            while find_image("sending.png", click=False, failsafe=False, scale=1.366):
+                print("Still sending, waiting...")
+                time.sleep(5)
+            while not find_image(
+                "camera.png",
+                roi_height_percentage=30,
+                roi_top_percentage=70,
+                failsafe=False,
+            ) and find_image("send.png", failsafe=False, click=False):
+                print("Still processing, waiting")
+            if not find_image(
+                "multi_snap_selected.png", click=False, failsafe=False, scale=1.373
+            ):
+                print("Multi-snap not enabled, enabling now...")
+                find_image("more.png")
+                time.sleep(1)
+                result = find_image("director_mode.png", click=False, scale=1.365)
+                if result:
+                    x, y = result
+                else:
+                    continue
+                device.shell(f"input touchscreen swipe {x} {y} {x} {y-500} 500")
+                device.shell(f"input touchscreen swipe {x} {y} {x} {y-500} 500")
+                device.shell(f"input touchscreen swipe {x} {y} {x} {y-500} 500")
+                if find_image("multi_snap.png", scale=1.362):
+                    print("Multi-snap enabled!")
+            if not find_image(
+                "edit_and_send.png",
+                failsafe=False,
+                roi_height_percentage=30,
+                roi_top_percentage=70,
+                scale=1.363,
+            ):
+                find_image("snap.png", click_times=7)
+                time.sleep(1)
+                find_image(
+                    "edit_and_send.png", roi_height_percentage=30, roi_top_percentage=70
+                )
+            time.sleep(1)
+            find_image("next.png", roi_height_percentage=30, roi_top_percentage=70)
+            time.sleep(1)
+            find_image("shortcut.png", roi_height_percentage=30, roi_top_percentage=0)
+            time.sleep(1)
+            find_image(
+                "select_all.png",
+                roi_height_percentage=30,
+                roi_top_percentage=0,
+                scale=1.354,
+            )
+            time.sleep(1)
+            find_image("send.png")
+            time.sleep(1)
+    except Exception as e:
+        print(f"Function a: Caught an exception - {e}")
+        traceback.print_exc()
+        send_snaps()
+
+
+def is_on_message_page():
+    frontpage_elements = ["camera_person.png", "snap_reply.png", "new_snap.png"]
+    for element in frontpage_elements:
+        result = find_image(element, failsafe=False, click=False)
+        if result is not False:
+            return True
+        return False
+
+
+def do_streaks():
+    scrolls_without_streaks = 0
+    height_percentage = 10
+    try:
+        while True:
+            result = find_image("streak.png", failsafe=False, click=False)
+            if result:
+                x, y = result
+                height, width = get_device_dimensions()
+                top_percentage = (y / height) * 100 - 5
+                if find_image(
+                    "delivered.png",
+                    failsafe=False,
+                    click=False,
+                    roi_height_percentage=height_percentage,
+                    roi_top_percentage=top_percentage,
+                ):
+                    print("Already delivered, moving on...")
+                    device.shell(f"input touchscreen swipe {x} {y} {x} {y-200} 100")
+                    continue
+                if find_image("new_snap.png", failsafe=False):
+                    device.shell(f"input touchscreen swipe {x/2} {y/2} {x/2} {y/2} 50")
+                result = find_image(
+                    "camera_person.png",
+                    failsafe=False,
+                    roi_height_percentage=height_percentage,
+                    roi_top_percentage=top_percentage,
+                ) or find_image(
+                    "snap_reply.png",
+                    failsafe=False,
+                    roi_height_percentage=height_percentage,
+                    roi_top_percentage=top_percentage,
+                )
+                if result:
+                    time.sleep(1)
+                    find_image("snap_single.png")
+                    time.sleep(1)
+                    find_image("send_direct.png")
+                else:
+                    result = find_image(
+                        "message_person.png",
+                        failsafe=False,
+                        roi_height_percentage=height_percentage,
+                        roi_top_percentage=top_percentage,
+                    )
+                    if result:
+                        time.sleep(1)
+                        find_image("camera_dms.png")
+                        time.sleep(1)
+                        find_image("snap_single.png")
+                        time.sleep(1)
+                        find_image("send_direct.png")
+                        time.sleep(1)
+                        find_image("exit_dms.png")
+                scrolls_without_adds = 0
+            else:
+                result = (
+                    find_image("camera_person.png", failsafe=False, click=False)
+                    or find_image("message_person.png", failsafe=False, click=False)
+                    or find_image("first_snap.png", failsafe=False, click=False)
+                )
+                if result:
+                    x, y = result
+                    device.shell(f"input touchscreen swipe {x} {y} {x} {y-200} 100")
+                    if not find_image("streak.png", failsafe=False):
+                        scrolls_without_streaks += 1
+                        if scrolls_without_streaks >= 10:
+                            print("No more streaks available, exiting...")
+                            break
+    except Exception as e:
+        print(f"Caught an exception - {e}")
+        traceback.print_exc()
+
+
+def add_friends():
+    find_image("friends.png", failsafe=False)
+    scrolls_without_adds = 0
+    try:
+        while True:
+            result = find_image("add.png", failsafe=False) or find_image(
+                "accept.png", failsafe=False
+            )
+            if not result:
+                result = find_image("message.png", failsafe=False, click=False)
+                if result:
+                    x, y = result
+                    device.shell(f"input touchscreen swipe {x} {y} {x} {y-200} 100")
+                    if not find_image("add.png", failsafe=False):
+                        scrolls_without_adds += 1
+                        if scrolls_without_adds >= 10:
+                            print("No more adds available, exiting...")
+                            break
+            else:
+                scrolls_without_adds = 0
+    except Exception as e:
+        print(f"Caught an exception - {e}")
+        traceback.print_exc()
+        add_friends()
+
+
+print("1. Send snaps")
+print("2. Add friends")
+print("3. Do streaks")
+print("4. Calibrate scale")
+choice = input("Enter your choice: ")
+if choice == "1":
+    send_snaps()
+elif choice == "2":
+    add_friends()
+elif choice == "3":
+    do_streaks()
+elif choice == "4":
+    calibrate_scale(input("Enter image name, press enter to continue: "))
+    print("Calibration complete")
---
+++ src/utils.py
@@ -0,0 +1,106 @@
+from ppadb.client import Client as AdbClient
+import cv2
+import time
+
+# Connect to ADB
+adb = AdbClient(host="127.0.0.1", port=5037)
+# Get ADB devices
+devices = adb.devices()
+# Check if at least one device is connected
+assert devices, "No ADB devices found."
+device = devices[0]
+
+# Check if the tap command was executed successfully
+output = device.shell("echo success")
+assert "success" in output, "Failed to execute adb command."
+
+attempts=0
+height = 0    
+width = 0
+previous_image = None
+previous_click_times = None
+
+show_roi = False
+
+
+def get_device():
+    return device
+
+def get_device_dimensions():
+    return height, width
+    
+def find_image(image_name, click = True, scale=1.36, click_times=1, roi_height_percentage=100, roi_top_percentage=0, failsafe=True):
+    global attempts, height, width, previous_image, previous_click_times
+    # Capture the screenshot
+    image = device.screencap()
+
+    # Save the screenshot as 'screen.png'
+    with open('screen.png', 'wb') as file:
+        file.write(image)
+
+    # Load the screenshot as an image in grayscale
+    screenshot = cv2.imread('screen.png', cv2.IMREAD_GRAYSCALE)
+
+
+    # Calculate the region of interest (ROI) based on the provided percentage values
+    height, width = screenshot.shape
+
+    roi_top = int(height * (roi_top_percentage / 100))
+    roi_height = int(height * (roi_height_percentage / 100))
+    roi_bottom = roi_top + roi_height
+
+    # Crop the screenshot to the defined ROI
+    screenshot_roi = screenshot[roi_top:roi_bottom, :]
+
+    # Load the template image to be recognized in grayscale
+    template_image = cv2.imread(f'./images/{image_name}', cv2.IMREAD_GRAYSCALE)
+
+    # List of scales to consider (adjust as needed)
+    threshold = 0.9
+
+    # Resize the template image
+    resized_template = cv2.resize(template_image, None, fx=scale, fy=scale)
+    if show_roi:
+        # Display the image
+        cv2.imshow('Image', screenshot_roi)
+        # Wait for a key event and close the window
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    # Perform template matching at the current scale
+    result = cv2.matchTemplate(screenshot_roi, resized_template, cv2.TM_CCOEFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+
+    # Check if a match was found at the current scale
+    if max_val > threshold:
+        # Get the coordinates of the top-left corner of the matched region
+        x, y = max_loc
+        y = y+(roi_top)
+        print(f"Found {image_name} at {x}, {y} with {max_val} certainty")
+        if failsafe is True:
+            attempts=0
+        previous_image = image_name
+        previous_click_times = click_times
+        # Click on the recognized image
+        if click:
+            for i in range(click_times):
+                device.shell(f'input touchscreen swipe {x} {y} {x} {y} 50')
+                if click_times>1:
+                    time.sleep(0.1)
+        return x, y
+    elif failsafe is True:
+        attempts=attempts+1
+        if attempts>=50:
+            device.shell("am force-stop com.snapchat.android")
+            device.shell("am start -n com.snapchat.android/com.snapchat.android.LandingPageActivity")
+            time.sleep(5)
+            raise("System broken, restarting...")
+        elif attempts % 10 == 0:
+            print(f"Failed to find {image_name}, moving to next step...")
+            return False
+        else:
+            if previous_image is not None and find_image(previous_image, failsafe=False, click=False):
+                print(f"Stuck on {previous_image}, attempting again, {attempts} attempts")
+                find_image(previous_image, failsafe=False, click_times=previous_click_times)
+            else:
+                print(f"{image_name} not found in the screenshot. Certainty of {max_val}, trying again, {attempts} attempts")
+            find_image(image_name, click=click, scale=scale, click_times=click_times, roi_height_percentage=roi_height_percentage, roi_top_percentage=roi_top_percentage, failsafe=failsafe)
